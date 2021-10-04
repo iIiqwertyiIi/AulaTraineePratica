c2f2cc7b94b573409bd20e880807e6a2a4087932
Leir-Cruz <gabrielcruzvazsantos@gmail.com>
2021-10-04 09:29:44 -0300

    metodo image_url para mandar mais de uma imagem

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,12 +1,22 @@
 class GameSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
-  attributes :id, :title ,:release, :platform, :genre, :photo_url
+  attributes :id, :title ,:release, :platform, :genre, :photo_url, :image_url
 
   has_many :expansions
 
   def photo_url
     if object.photo.attached?
       rails_blob_path(object.photo, only_path: true)
+    else
+      nil
+    end
+  end
+
+  def image_url
+    if object.images.attached?
+      object.images.map do |image|
+        rails_blob_path(image, only_path: true)
+      end
     else
       nil
     end
